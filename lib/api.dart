de92554a9e473029a92eaaa0d8efd7eba1dcef1f
Murf-y <charbelfayad64@gmail.com>
2022-09-03 14:03:38 +0300

    change api url

--- lib/api.dart
+++ lib/api.dart
@@ -1,6 +1,6 @@
 import 'package:http/http.dart' as http;
 
 Future<http.Response> requestDollarRate() async {
-  String api = 'https://LBP-DollarRate-API.murfyy.repl.co/api/v1/dollarRate';
+  String api = 'https://rate.onrender.com/api/v1/dollarRate';
   return http.get(Uri.parse(api));
 }
